7d1a3b603d6daffdb6ff1f12196bccbabb0d216b
Erdni Zungruev <shagidzun@gmail.com>
2023-11-06 21:45:49 +0300

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,26 +1,27 @@
-let cart = [{
-  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 2
-},
-{
-  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1
-}];
+let cart = JSON.parse(localStorage.getItem('cart'));
+
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 function addToCart(productId, itemQuantity) {
   const foundItem = cart.find((cartItem) => cartItem.productId === productId);
   if (!foundItem) {
-        cart.push({
-        productId: productId,
-        quantity: itemQuantity
-        });
-      } else {
-        foundItem.quantity += itemQuantity;
-      }
+    cart.push({
+    productId: productId,
+    quantity: itemQuantity
+    });
+  } else {
+    foundItem.quantity += itemQuantity;
+  }
+
+  saveToStorage();
 };
 
 function removeFromCart(productId) {
   cart = cart.filter((cartItem) => cartItem.productId !== productId);
+  saveToStorage();
 }
 
 export { cart, addToCart, removeFromCart };
